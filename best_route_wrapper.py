1cd1fcb4747385cd9e07f44dbc6e1f277088d533
Pedro Silva <silvapst@gmail.com>
2022-11-28 15:21:43 -0300

    Languages changes
    translated all variables from Portuguese to English

--- best_route_wrapper.py
+++ best_route_wrapper.py
@@ -1,21 +1,19 @@
-from graph import Graph
 from dijkstra import dijkstra_algorithm
 
 class Best_route_wrapper(object):
 
-    def best_route(self, grafo, ponto_de_saida, ponto_de_chegada):
-        previous_nodes, shortest_path = dijkstra_algorithm(grafo, ponto_de_saida)
+    def best_route(self, graph, starting_point, destination):
+        previous_nodes, shortest_path = dijkstra_algorithm(graph, starting_point)
         
-        caminho = []
-        node = ponto_de_chegada
+        path = []
+        node = destination
 
-        while node != ponto_de_saida:
-            caminho.append(node)
+        while node != starting_point:
+            path.append(node)
             node = previous_nodes[node]
 
-    # Add the start node manually
-        caminho.append(ponto_de_saida)
+        path.append(starting_point)
 
         print("Minha joia, o caminho mais curto é de {} km, através desses vértices:".format(
-            shortest_path[ponto_de_chegada]))
-        print(" -> ".join(reversed(caminho)))    
+            shortest_path[destination]))
+        print(" -> ".join(reversed(path)))    
