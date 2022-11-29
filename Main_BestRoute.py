085e3470404cead85d4d9146e21ad50a1a562aea
Shell <shelldemoura@gmail.com>
2022-11-28 22:02:24 -0300

    Add methods calculator and best route
    Implementation methods calculator and best route for truck.

--- Main_BestRoute.py
+++ Main_BestRoute.py
@@ -1,32 +1,32 @@
 from graph import Graph
-from dijkstra import dijkstra_algorithm, print_result
+from best_route_wrapper import *
+from Truck import *
 
-nodes = ["V1", "V2", "V3", "V4",
-         "V5", "V6", "V7", "V8"]
+nodes = ["D1", "C2", "V3", "V4",
+         "D5", "C6", "V7", "V8"]
 
 init_graph = {}
 for node in nodes:
     init_graph[node] = {}
 
-init_graph["V1"]["V2"] = 33
-init_graph["V1"]["V4"] = 41
-init_graph["V1"]["V7"] = 15
-init_graph["V2"]["V3"] = 18
-init_graph["V2"]["V7"] = 11
-init_graph["V3"]["V6"] = 37
+init_graph["D1"]["C2"] = 33
+init_graph["D1"]["V4"] = 41
+init_graph["D1"]["V7"] = 15
+init_graph["C2"]["V3"] = 18
+init_graph["C2"]["V7"] = 11
+init_graph["V3"]["C6"] = 37
 init_graph["V3"]["V7"] = 32
 init_graph["V3"]["V8"] = 26
-init_graph["V4"]["V5"] = 7
+init_graph["V4"]["D5"] = 7
 init_graph["V4"]["V7"] = 27
 init_graph["V4"]["V8"] = 13
-init_graph["V5"]["V6"] = 27
-init_graph["V5"]["V8"] = 25
-init_graph["V6"]["V8"] = 10
+init_graph["D5"]["C6"] = 27
+init_graph["D5"]["V8"] = 25
+init_graph["C6"]["V8"] = 10
 
 g = Graph(nodes, init_graph)
-# print(g.get_nodes())
-# gerar o melhor partindo de um determinado ponto
-previous_nodes, shortest_path = dijkstra_algorithm(g, "V3")
-#print(dijkstra_algorithm(g, "V2"))
-#
-print(print_result(previous_nodes, shortest_path, "V3", "V5"))
+brw = Best_route_wrapper()
+truck = Truck("123", False, "V4")
+
+brw.best_route(g, truck)
+
