f8b3948a11da77389a3498494aa04bc5271bfe8f
Shell <shelldemoura@gmail.com>
2022-11-28 22:02:24 -0300

    Add methods calculator and best route
    Implementation methods calculator and best route for truck.

--- best_route_wrapper.py
+++ best_route_wrapper.py
@@ -1,21 +1,42 @@
-from graph import Graph
 from dijkstra import dijkstra_algorithm
+
 
 class Best_route_wrapper(object):
 
-    def best_route(self, grafo, ponto_de_saida, ponto_de_chegada):
-        previous_nodes, shortest_path = dijkstra_algorithm(grafo, ponto_de_saida)
-        
-        caminho = []
-        node = ponto_de_chegada
+    def route_calculator(self, graph, starter_node, target_node):
+        previous_nodes, shortest_path = dijkstra_algorithm(graph, starter_node)
 
-        while node != ponto_de_saida:
-            caminho.append(node)
+        path = []
+        node = target_node
+
+        while node != starter_node:
+            path.append(node)
             node = previous_nodes[node]
 
     # Add the start node manually
-        caminho.append(ponto_de_saida)
+        path.append(starter_node)
+        return path, shortest_path[target_node]
 
-        print("Minha joia, o caminho mais curto é de {} km, através desses vértices:".format(
-            shortest_path[ponto_de_chegada]))
-        print(" -> ".join(reversed(caminho)))    
+    def best_route(self, g, truck):
+        if truck.get_status():
+            pathD1, distanceD1 = self.route_calculator(
+                g, truck.get_localization(), "D1")
+            pathD5, distanceD5 = self.route_calculator(
+                g, truck.get_localization(), "D5")
+            if distanceD1 <= distanceD5:
+                print('We found the following best path: ',
+                      ' -> '.join(reversed(pathD1)))
+            else:
+                print('We found the following best path: ',
+                      ' -> '.join(reversed(pathD5)))
+        else:
+            pathC2, distanceC2 = self.route_calculator(
+                g, truck.get_localization(), "C2")
+            pathC6, distanceC6 = self.route_calculator(
+                g, truck.get_localization(), "C6")
+            if distanceC2 <= distanceC6:
+                print('We found the following best path: ',
+                      ' -> '.join(reversed(pathC2)))
+            else:
+                print('We found the following best path: ',
+                      ' -> '.join(reversed(pathC6)))
